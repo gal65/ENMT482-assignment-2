3be4bb7eb0c87cafc4287f2c4c746eb9b9309ca7
Gordon Lay <gal65@uclive.ac.nz>
2021-10-07 14:16:59 +1300

    dsoigoiknbewONEWBK

--- poi_transformations.py
+++ poi_transformations.py
@@ -1,7 +1,16 @@
 # Transforms for points of interaction in their respective machine frames
+# Includes offset transformation
 
 import numpy as np
 from transformation_matrices import transform_rotx
 from transformation_matrices import transform_roty
 from transformation_matrices import transform_rotz
 
+def espresso_button():
+    D_BUT
+
+def grinder_PF2():
+
+def grinder_button():
+
+def 
